9ce2166d22bf5ec84c6ac4d80ac66ca84179dedf
liqiushi <li.qius@husky.neu.edu>
2020-05-25 14:03:44 -0400

    add DateTime and Util classes, update Visit class

--- GroceryStoryTraffic/src/main/java/model/Visit.java
+++ GroceryStoryTraffic/src/main/java/model/Visit.java
@@ -8,9 +8,9 @@ public class Visit {
   // I just remembered that LocalDate does not include time data
   // Will need to think about how to represent the exact time data.
   // Using String as a placeholder for now.
-  // Todo (James): Please update the type here after we figure out how to represent time.
-  private String entryTime;
-  private String leaveTime;
+  // Updated (James): switch to self-defined DateTime class.
+  private DateTime entryTime;
+  private DateTime leaveTime;
 
   // Will calculate this based on leave time - entry time.
   private Integer totalTime;
